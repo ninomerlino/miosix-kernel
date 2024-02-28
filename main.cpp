22e6c831581058e69dde5be2fcfb3099f8f3648b
fliuzzi02 <fliuzzi02@gmail.com>
2024-02-28 14:38:12 +0100

    modified ready to compile

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
+#include <miosix.h>
 
-#include <cstdio>
-#include "miosix.h"
-
-using namespace std;
 using namespace miosix;
 
 int main()
 {
-    //iprintf("Hello world, write your application here\n");
+    for(;;)
+    {
+        ledOn();
+        Thread::sleep(1000);
+        ledOff();
+        Thread::sleep(1000);
+    }
 }
